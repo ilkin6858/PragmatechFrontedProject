3c8249f2b7abdbd7d6b6ac660cce6c0d719b556d
ilkin qasimov <ilkin6858@gmail.com>
2022-03-14 23:05:35 +0400

    update

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,2 +1,18 @@
-let str=Number("hello world");
-console.log(7/0)
+// "use strict"; butun kodlari use strcit edir
+
+// ad="qasimov";
+// console.log(ad);
+
+
+
+// function show(){
+//     "use strict"; // sadece funksiya icerisinde yazilan kodlar use stcrit olur
+//    let  a=4;
+//     console.log(a)
+// };
+// show()
+
+
+x=10;
+console.log(x);
+delete x;
