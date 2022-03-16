01c36abccb6c71931b7ad47745d8bef17b67dd00
ilkin qasimov <ilkin6858@gmail.com>
2022-03-17 00:00:52 +0400

    update

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -13,6 +13,36 @@
 // show()
 
 
-x=10;
-console.log(x);
-delete x;
+// x=10;
+// console.log(x);
+// delete x;
+
+// let a=20;
+// console.log(a/0);
+// console.log(typeof a)
+
+// let ad='ilkin';
+// let eded=20;
+// console.log(ad/eded);
+// console.log(typeof NaN);
+
+// let ad="ilkin"
+// console.log(`salam:${"dostlar"}`);
+
+// let a="abcdgftrd";
+// console.log(a.length-1);
+
+// let a="10";
+// let b=20;
+// console.log(Number(a)+b);
+
+
+// let a="20";
+// let b=30;
+// console.log(+a+b)
+
+// let a=20;
+// let b="30";
+// console.log(a + +b)
+
+
